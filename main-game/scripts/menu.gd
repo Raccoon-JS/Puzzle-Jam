b5508cd1ef2dc8695e4f97517514760525e6aca2
= <raccoon_js@protonmail.com>
2019-06-07 23:18:14 -0500

    menu screen 90% finished.

--- main-game/scripts/menu.gd
+++ main-game/scripts/menu.gd
@@ -1,4 +1,63 @@
 extends Node
 
 func _ready():
+	
+	pass # Replace with function body.
+
+func change_black():
+	$background/background_color.color = Color(0,0,0,1)
+	$background/title.add_color_override("font_color", Color(1,1,1,1))
+	pass
+
+func reverse_change():
+	$background/background_color.color = Color(1,1,1,1)
+	$background/title.add_color_override("font_color", Color(0,0,0,1))
+	pass
+
+# Pressed
+func _on_start_button_pressed():
+	$start.visible = false
+	$about.visible = false
+	$quit.visible = false
+	$player.play("sucked_into_tv")
+	pass # Replace with function body.
+
+func _on_about_button_pressed():
+	print("about")
+	pass # Replace with function body.
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+# Start Button Color Change
+func _on_start_button_mouse_entered():
+	change_black()
+	pass # Replace with function body.
+
+func _on_start_button_mouse_exited():
+	reverse_change()
+	pass # Replace with function body.
+
+# About Button Color Change
+func _on_about_button_mouse_entered():
+	change_black()
+	pass # Replace with function body.
+
+func _on_about_button_mouse_exited():
+	reverse_change()
+	pass # Replace with function body.
+
+# Quit Button Color Change
+func _on_quit_button_mouse_entered():
+	change_black()
+	pass # Replace with function body.
+
+func _on_quit_button_mouse_exited():
+	reverse_change()
+	pass # Replace with function body.
+
+func _on_player_animation_finished(anim_name):
+	if anim_name == "sucked_into_tv":
+		global.goto_scene("res://scenes/intro.tscn")
 	pass # Replace with function body.
--- main-game/scripts/opening.gd
+++ main-game/scripts/opening.gd
@@ -4,8 +4,14 @@ func _ready():
 	$animation.play("fade_logo")
 	pass # Replace with function body.
 
+func _input(event):
+	if event.is_action_pressed("left_click") or Input.is_key_pressed(KEY_ENTER):
+		global.goto_scene("res://scenes/menu.tscn")
+	pass
 
 func _on_animation_finished(anim_name):
 	if anim_name == "fade_logo":
 		$animation.play("white_out")
+	if anim_name == "white_out":
+		global.goto_scene("res://scenes/menu.tscn")
 	pass # Replace with function body.
